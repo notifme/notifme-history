df338fe116caac03975e59775b1490e7caa2dac0
David Brown <david@devanco.com>
2017-09-29 17:35:45 +0200

    add standard linter

--- client/main.js
+++ client/main.js
@@ -1,22 +1,22 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {Template} from 'meteor/templating'
+import {ReactiveVar} from 'meteor/reactive-var'
 
-import './main.html';
+import './main.html'
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.hello.onCreated(function helloOnCreated () {
   // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+  this.counter = new ReactiveVar(0)
+})
 
 Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+  counter () {
+    return Template.instance().counter.get()
+  }
+})
 
 Template.hello.events({
-  'click button'(event, instance) {
+  'click button' (event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+    instance.counter.set(instance.counter.get() + 1)
+  }
+})
